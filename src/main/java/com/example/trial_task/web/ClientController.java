74b53b125eef4ec09bbde843c926b5894544c974
Alexey Boyarinov <boyarinovalexey@gmail.com>
2024-09-17 15:31:04 +0300

    Added Open API (Swagger)

--- src/main/java/com/example/trial_task/web/ClientController.java
+++ src/main/java/com/example/trial_task/web/ClientController.java
@@ -2,7 +2,14 @@ package com.example.trial_task.web;
 
 import com.example.trial_task.dto.ClientCreationDTO;
 import com.example.trial_task.dto.ClientDTO;
-import com.example.trial_task.service.ClientService;
+import com.example.trial_task.service.IClientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
@@ -16,13 +23,21 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping(path = ClientController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+@Tag(name = "Client API", description = "Operations related to managing clients")
 public class ClientController {
     public static final String REST_URL = "/api/v1/clients";
-    
+
     private final IClientService clientService;
 
     @PostMapping
-    public ResponseEntity<ClientDTO> add(@RequestBody  @Valid ClientCreationDTO client) {
+    @Operation(summary = "Create a new client")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Client created successfully",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ClientDTO.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid input data"),
+            @ApiResponse(responseCode = "404", description = "Client not found")
+    })
+    public ResponseEntity<ClientDTO> add(@RequestBody @Valid ClientCreationDTO client) {
         ClientDTO created = clientService.create(client);
         URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
                 .path(REST_URL + "/{id}")
@@ -32,13 +47,21 @@ public class ClientController {
     }
 
     @GetMapping
+    @Operation(summary = "Return a list of clients")
     public ResponseEntity<List<ClientDTO>> getAll() {
         List<ClientDTO> clients = clientService.getAll();
         return ResponseEntity.ok(clients);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ClientDTO> getById(@PathVariable Integer id) {
+    @Operation(summary = "Get a client by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Client data received successfully"),
+            @ApiResponse(responseCode = "404", description = "Client not found")
+    })
+    public ResponseEntity<ClientDTO> getById(
+            @Parameter(description = "ID of the client", required = true) @PathVariable Integer id
+    ) {
         ClientDTO client = clientService.getById(id);
         return ResponseEntity.ok(client);
     }
--- src/main/java/com/example/trial_task/web/ContactController.java
+++ src/main/java/com/example/trial_task/web/ContactController.java
@@ -3,7 +3,14 @@ package com.example.trial_task.web;
 import com.example.trial_task.dto.ContactCreationDTO;
 import com.example.trial_task.dto.ContactDTO;
 import com.example.trial_task.model.ContactType;
-import com.example.trial_task.service.ContactService;
+import com.example.trial_task.service.IContactService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
@@ -18,14 +25,25 @@ import java.util.Map;
 @RestController
 @AllArgsConstructor
 @RequestMapping(path = ContactController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+@Tag(name = "Contact API", description = "Operations related to managing contacts")
 public class ContactController {
     public static final String REST_URL = "/api/v1/clients/{id}/contacts";
 
     private final IContactService contactService;
 
+    @Operation(summary = "Create a new contact")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Contact created successfully",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ContactDTO.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid input data"),
+            @ApiResponse(responseCode = "404", description = "Client not found")
+    })
     @PostMapping
-    public ResponseEntity<ContactDTO> add(@RequestBody @Valid ContactCreationDTO contact) {
-        ContactDTO created = contactService.create(contact);
+    public ResponseEntity<ContactDTO> add(
+            @RequestBody @Valid ContactCreationDTO contact,
+            @Parameter(description = "ID of the client", required = true) @PathVariable Integer id
+    ) {
+        ContactDTO created = contactService.create(contact, id);
         URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
                 .path(REST_URL + "/{id}")
                 .buildAndExpand(created.id())
@@ -33,15 +51,28 @@ public class ContactController {
         return ResponseEntity.created(uriOfNewResource).body(created);
     }
 
+    @Operation(summary = "Get all contacts by client ID", description = "Retrieve all contacts grouped by type for a given client ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Contacts retrieved successfully"),
+            @ApiResponse(responseCode = "404", description = "Client not found")
+    })
     @GetMapping
-    public ResponseEntity<Map<ContactType, List<ContactDTO>>> getAllByClient(@RequestParam Integer clientId) {
-        Map<ContactType, List<ContactDTO>> contacts = contactService.getAllByClientId(clientId);
+    public ResponseEntity<Map<ContactType, List<ContactDTO>>> getAllByClient(
+            @Parameter(description = "ID of the client", required = true) @PathVariable Integer id) {
+        Map<ContactType, List<ContactDTO>> contacts = contactService.getAllByClientId(id);
         return ResponseEntity.ok(contacts);
     }
 
+    @Operation(summary = "Get contacts by client ID and contact type", description = "Retrieve all contacts of a specific type for a given client ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Contacts retrieved successfully"),
+            @ApiResponse(responseCode = "404", description = "Client or contact type not found"),
+    })
     @GetMapping("/{type}")
-    public ResponseEntity<List<ContactDTO>> getAllByTypeAndClient(@PathVariable String type, @RequestParam Integer clientId) {
-        List<ContactDTO> contacts = contactService.getAllByTypeAndClientId(type, clientId);
+    public ResponseEntity<List<ContactDTO>> getAllByClientAndType(
+            @Parameter(description = "Type of the contact", required = true) @PathVariable String type,
+            @Parameter(description = "ID of the client", required = true) @PathVariable Integer id) {
+        List<ContactDTO> contacts = contactService.getAllByTypeAndClientId(type, id);
         return ResponseEntity.ok(contacts);
     }
 }
